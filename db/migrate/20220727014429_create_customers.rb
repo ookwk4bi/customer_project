8f260f40bdeff40a75832d150b46feb5b87ba5bb
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-27 10:50:17 +0900

    first-commit

---
+++ db/migrate/20220727014429_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :family_name, null: false, length: { maximum: 20 }
+      t.string :given_name,  null: false, length: { maximum: 20 }
+      t.text :detail,       null: false
+      t.string :account_number, null: false
+
+      t.timestamps
+    end
+  end
+end
