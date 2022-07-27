d764c7656c340b1b9d3ec7f8e05e9d6c58adce9a
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-27 15:09:53 +0900

    修正２

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,23 +1,26 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
   
-
   def index
     # @customers = Customer.page(params[:page])
     if params[:q]&.dig(:family_name)
       squished_keywords = params[:q][:family_name].squish
-      params[:q][:family_name_cont_any] = squished_keywords.split(' ')
+      params[:q][:family_name_cont_any] = squished_keywords.split(" ")
     end
     if params[:q]&.dig(:given_name)
       squished_keywords = params[:q][:given_name].squish
-      params[:q][:given_name_cont_any] = squished_keywords.split(' ')
+      params[:q][:given_name_cont_any] = squished_keywords.split(" ")
     end
     if params[:q]&.dig(:account_number)
       squished_keywords = params[:q][:account_number].squish
-      params[:q][:account_number_cont_any] = squished_keywords.split(' ')
+      params[:q][:account_number_cont_any] = squished_keywords.split(" ")
     end
     @q = Customer.ransack(params[:q])
     @customers = @q.result.page(params[:page])
   end
+  
+  
 
   def new
     @customer = Customer.new
@@ -26,45 +29,44 @@ class CustomersController < ApplicationController
   def create
     @customer = Customer.new(customer_params)
     if @customer.save
-      redirect_to @customer
+    redirect_to @customer
     else
       render :new
-    end
   end
+end
 
   def edit
-    @customer = Customer.find(params[:id])
+    
   end
 
   def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+    
+   if @customer.update(customer_params)
+    redirect_to root_path
+   else
+    render :edit
+   end
   end
 
   def show
-    @customer = Customer.find(params[:id])
     @comment = Comment.new
     # @comments = Comment.where(customer_id: params[:id].to_i)
     @comments = @customer.comments
   end
 
   def destroy
-    @customer = Customer.find(params[:id])
+    
     @customer.destroy
     redirect_to root_path
   end
 
   def search
     if params[:q]&.dig(:name)
-      squished_keywords = params[:q][:name].squish
-      params[:q][:name_cont_any] = squished_keywords.split(' ')
-    end
-    @q = Item.ransack(params[:q])
-    @customers = @q.result
+    squished_keywords = params[:q][:name].squish
+    params[:q][:name_cont_any] = squished_keywords.split(" ")
+  end
+  @q = Item.ransack(params[:q])
+  @customers = @q.result
   end
 
   private
@@ -72,10 +74,13 @@ class CustomersController < ApplicationController
   def customer_params
     params.require(:customer).permit(
       :family_name,
-      :given_name,
-      :detail,
+      :given_name, 
+      :detail, 
       :account_number
-    )
-  end
+      )
+  end  
 
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
 end
