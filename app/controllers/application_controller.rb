1c2459d868d34de2b3583f4e9d4557bcc75c1267
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-07-27 13:34:33 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
-before_action :configure_permitted_parameters, if: :devise_controller?
-protect_from_forgery
+ before_action :configure_permitted_parameters, if: :devise_controller?
+ protect_from_forgery
     
   
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
+ def configure_permitted_parameters
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:given_name])
         
-        devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:given_name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:given_name])
         
-    end
+ end
 
 end
-
