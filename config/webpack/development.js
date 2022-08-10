6a529f4d1832e17041b70f7675052149264a36e4
ookwk4bi <106803644+ookwk4bi@users.noreply.github.com>
2022-08-10 21:02:38 +0900

    c

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-module.exports = environment.towebpackConfig()
+module.exports = environment.toWebpackConfig()
